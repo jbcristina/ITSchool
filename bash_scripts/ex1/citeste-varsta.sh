44e85f4dd47bf0ce9a50bbe50ab87d7e9b024772
Jiga-Boeriu Cristina <cristina@yahoo.com>
2025-06-17 14:08:24 +0300

    Am adaugat scripturile din bash_scripts/ex1

---
+++ bash_scripts/ex1/citeste-varsta.sh
@@ -0,0 +1,7 @@
+
+# Cere userului sa introduca o varsta 
+# Ce a introdus userul este pus in variabila $varsta
+
+read -p "Introduceti varsta: " varsta
+
+echo Varsta introdusa este: $varsta
---
+++ bash_scripts/ex1/non-interact-source.sh
@@ -0,0 +1,19 @@
+
+# chmod +x non-interactiv-source.sh
+
+# ./non-interactiv-source.sh
+# Observam pid-ul si incercam sa rulam
+# echo CURSANT
+
+# Apoi rulam cu source
+# source ./non-interactiv-source.sh
+# Observam pid-ul si incercam sa rulam
+# echo CURSANT
+
+echo "PID-ul procesului curent este $$"
+
+export CURSANT="jbcristina"
+
+#cat /proc/$$/status | grep -i name
+#echo $$
+
