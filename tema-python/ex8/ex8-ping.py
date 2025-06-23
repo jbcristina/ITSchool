ea6221edee24946abffae41232a31954353072aa
Jiga-Boeriu Cristina <cristina@yahoo.com>
2025-06-23 19:16:46 +0300

    Am modificat scriptul de la ex8

--- tema-python/ex8/ex8-ping.py
+++ tema-python/ex8/ex8-ping.py
@@ -4,8 +4,10 @@ import subprocess
 try:
     # Folosește subprocess pentru a executa comanda
     # result = subprocess.run(["ping", "-c", "1", "8.8.8.8"], capture_output=True, text=True, check=True)
-    # result = subprocess.run(["ls", "-l", "/etc"], capture_output=True, text=True, check=True)
-    result = subprocess.run(["cat", "/etc"], capture_output=True, text=True, check=True)
+    result = subprocess.run(["ls", "-alh", "/home/alice"], capture_output=True, text=True, check=True)
+    # result = subprocess.run(["cat", "/etc"], capture_output=True, text=True, check=True)
+    # result = subprocess.run(["ping -c 1 8.8.8.8"], capture_output=True, text=True, check=True)
+    # result = subprocess.Popen(["sleep", "999"])
     print("Comanda a reușit!")
     print("Ieșire:", result.stdout)
 except subprocess.CalledProcessError as e:
